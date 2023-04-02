09e992928feaaa17a42d99b07b04b84c1092a6f2
BrnMarq <BrnMarq@gmail.com>
2023-04-02 02:19:07 -0400

    🎉 Begin Project!

---
+++ TuProductoOnline/LoginForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuProductoOnline
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+            VerifyInputs();
+        }
+
+        private void txtClave_TextChanged(object sender, EventArgs e)
+        {
+            VerifyInputs();
+        }
+
+        private void VerifyInputs()
+        {
+            //Verificaciones de que los campos del usuario y clave no esten vacíos 
+            //y asi activar el boton de acceso
+            if (string.IsNullOrEmpty(txtUsuario.Text) || string.IsNullOrEmpty(txtClave.Text))
+            {
+                btnAcceso.Enabled = false;
+                return;
+            }
+            btnAcceso.Enabled = true;
+        }
+    }
+}
