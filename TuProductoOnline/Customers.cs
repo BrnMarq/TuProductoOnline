cf7141f0c7077768299e313b9210ec6361c94fd1
BrnMarq <BrnMarq@gmail.com>
2023-04-03 16:55:44 -0400

    Add: customers interface

---
+++ TuProductoOnline/Customers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TuProductoOnline
+{
+    public partial class Customers : TuProductoOnline.NavBar
+    {
+       
+        public Customers()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+            customerProperties customerWindow = new customerProperties();
+            customerWindow.Show();
+        }
+
+        private void btnCheckCustomer_Click(object sender, EventArgs e)
+        {
+            customerProperties customerWindow = new customerProperties();
+            customerWindow.Show();
+        }
+    }
+}
--- TuProductoOnline/Properties/Resources.Designer.cs
+++ TuProductoOnline/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace TuProductoOnline.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -76,6 +76,46 @@ namespace TuProductoOnline.Properties {
         internal static System.Drawing.Bitmap ClientsIcon {
             get {
                 object obj = ResourceManager.GetObject("ClientsIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap deleteIcon {
+            get {
+                object obj = ResourceManager.GetObject("deleteIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap deleteIcon1 {
+            get {
+                object obj = ResourceManager.GetObject("deleteIcon1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap editIcon {
+            get {
+                object obj = ResourceManager.GetObject("editIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap editIcon1 {
+            get {
+                object obj = ResourceManager.GetObject("editIcon1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
