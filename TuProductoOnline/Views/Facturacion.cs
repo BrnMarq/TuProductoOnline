73f458b548fcf6621d7dcc0df8988bf51e20c68e
Roth <am.apostol17@gmail.com>
2023-04-05 14:20:45 -0400

    Factura: Añadir listas de listas

--- TuProductoOnline/Views/Facturacion.cs
+++ TuProductoOnline/Views/Facturacion.cs
@@ -13,11 +13,22 @@ namespace TuProductoOnline.Views
     
     public partial class Facturacion : Form
     {
-        
+        List<List<string>> Clientes = new List<List<string>>();
+        List<List<string>> Productos = new List<List<string>>();
+        List<List<string>> ProductosCarrito = new List<List<string>>();
+
         public Facturacion()
         {
             InitializeComponent();
-            
+            Clientes.Add(new List<string> { "007", "Angel", "5" });
+            Clientes.Add(new List<string> { "2", "Franchesco", "3" });
+            Clientes.Add(new List<string> { "3", "Virgolini", "1" });
+            Clientes.Add(new List<string> { "4", "TuNoMeteCabraZalamanbique", "5" });
+
+            foreach (List<string> subList in Clientes)
+            {
+                ClientBox1.Items.Add(subList[1]);
+            }
         }
 
         private void Facturacion_Load(object sender, EventArgs e)
