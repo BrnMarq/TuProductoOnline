1c8fc019169fdeeb84013f784105eb79f8a8e7f6
María Castillo <mafercasmen@gmail.com>
2023-04-05 15:20:03 -0430

    Add: customer class

--- TuProductoOnline/customerProperties.cs
+++ TuProductoOnline/customerProperties.cs
@@ -7,11 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TuProductoOnline.Models;
 
 namespace TuProductoOnline
 {
     public partial class CustomerProperties : Form
-    {
+    { 
         public CustomerProperties()
         {
             InitializeComponent();
@@ -19,6 +20,17 @@ namespace TuProductoOnline
 
         private void btnAccept_Click(object sender, EventArgs e)
         {
+            Customer cliente = new Customer();
+
+            cliente.IncrementarCode();
+            cliente.Name = txtName.Text;
+            cliente.LastName = txtLastName.Text;
+            cliente.Document = txtId.Text;
+            cliente.PhoneNumber = txtPhoneNumber.Text;
+            cliente.Direction = txtDirection.Text;
+            cliente.Email = txtEmail.Text;
+            cliente.Type = cbType.SelectedItem.ToString();
+
             this.Close(); 
         }
     }
