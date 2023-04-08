5c19e647fc7993557bc7bdedaf5b60f8c00ba356
Roth <am.apostol17@gmail.com>
2023-04-08 00:08:58 -0400

    Factura V2

--- TuProductoOnline/Views/Facturacion.Designer.cs
+++ TuProductoOnline/Views/Facturacion.Designer.cs
@@ -39,9 +39,21 @@
             this.btnAgregarCarrito = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.ProductBox2 = new System.Windows.Forms.ComboBox();
-            this.btnFacturar = new System.Windows.Forms.Button();
             this.txtDgv = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.ListProducTable = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnFacturar = new System.Windows.Forms.Button();
+            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.txtTituloSubTotal = new System.Windows.Forms.Label();
+            this.txtTituloTotal = new System.Windows.Forms.Label();
+            this.txtSubTotal = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.Label();
+            this.groupBoxSubtotal = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.ClientBox.SuspendLayout();
             this.ProductBox.SuspendLayout();
             this.SuspendLayout();
@@ -95,11 +107,12 @@
             this.ClientBox1.Name = "ClientBox1";
             this.ClientBox1.Size = new System.Drawing.Size(121, 21);
             this.ClientBox1.TabIndex = 0;
-            this.ClientBox1.Text = "- Seleccionar -";
             // 
             // ProductBox
             // 
             this.ProductBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProductBox.Controls.Add(this.btnAgregarAlCarrito);
+            this.ProductBox.Controls.Add(this.bntAñadirProduct);
             this.ProductBox.Controls.Add(this.txtCantidad);
             this.ProductBox.Controls.Add(this.CantidadBox);
             this.ProductBox.Controls.Add(this.btnAgregarCarrito);
@@ -159,6 +172,85 @@
             this.ProductBox2.TabIndex = 0;
             this.ProductBox2.Text = "- Seleccionar -";
             // 
+            this.ListProducTable.AllowUserToAddRows = false;
+            this.ListProducTable.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListProducTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ListProducTable.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.ListProducTable.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ListProducTable.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ListProducTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.ListProducTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListProducTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.name,
+            this.precio,
+            this.Cantidad,
+            this.delete});
+            this.ListProducTable.EnableHeadersVisualStyles = false;
+            this.ListProducTable.Location = new System.Drawing.Point(12, 125);
+            this.ListProducTable.Name = "ListProducTable";
+            this.ListProducTable.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.RoyalBlue;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ListProducTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.ListProducTable.RowHeadersVisible = false;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            this.ListProducTable.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.ListProducTable.RowTemplate.DefaultCellStyle.NullValue = "1";
+            this.ListProducTable.RowTemplate.DividerHeight = 2;
+            this.ListProducTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ListProducTable.Size = new System.Drawing.Size(584, 235);
+            this.ListProducTable.TabIndex = 7;
+            this.ListProducTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ListProducTable_CellContentClick);
+            // 
+            // ID
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.ID.DefaultCellStyle = dataGridViewCellStyle2;
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Nombre";
+            this.name.Name = "name";
+            this.name.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // precio
+            // 
+            this.precio.HeaderText = "Precio";
+            this.precio.Name = "precio";
+            this.precio.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // delete
+            // 
+            this.delete.HeaderText = "Eliminar";
+            this.delete.Name = "delete";
+            this.delete.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.delete.Text = "Eliminar";
+            // 
             // btnFacturar
             // 
             this.btnFacturar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
@@ -174,12 +266,82 @@
             // 
             // txtDgv
             // 
-            this.txtDgv.AutoSize = true;
-            this.txtDgv.Location = new System.Drawing.Point(16, 106);
-            this.txtDgv.Name = "txtDgv";
-            this.txtDgv.Size = new System.Drawing.Size(97, 13);
-            this.txtDgv.TabIndex = 6;
-            this.txtDgv.Text = "Lista de productos:";
+            this.dataGridViewImageColumn1.HeaderText = "Eliminar";
+            this.dataGridViewImageColumn1.Image = global::TuProductoOnline.Properties.Resources.deleteIcon1;
+            this.dataGridViewImageColumn1.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
+            this.dataGridViewImageColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewImageColumn1.Width = 117;
+            // 
+            // txtTituloSubTotal
+            // 
+            this.txtTituloSubTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtTituloSubTotal.AutoSize = true;
+            this.txtTituloSubTotal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.txtTituloSubTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTituloSubTotal.ForeColor = System.Drawing.Color.White;
+            this.txtTituloSubTotal.Location = new System.Drawing.Point(3, 10);
+            this.txtTituloSubTotal.Name = "txtTituloSubTotal";
+            this.txtTituloSubTotal.Size = new System.Drawing.Size(82, 20);
+            this.txtTituloSubTotal.TabIndex = 18;
+            this.txtTituloSubTotal.Text = "Subtotal:";
+            // 
+            // txtTituloTotal
+            // 
+            this.txtTituloTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtTituloTotal.AutoSize = true;
+            this.txtTituloTotal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.txtTituloTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTituloTotal.ForeColor = System.Drawing.Color.White;
+            this.txtTituloTotal.Location = new System.Drawing.Point(8, 7);
+            this.txtTituloTotal.Name = "txtTituloTotal";
+            this.txtTituloTotal.Size = new System.Drawing.Size(54, 20);
+            this.txtTituloTotal.TabIndex = 19;
+            this.txtTituloTotal.Text = "Total:";
+            // 
+            // txtSubTotal
+            // 
+            this.txtSubTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSubTotal.AutoSize = true;
+            this.txtSubTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSubTotal.Location = new System.Drawing.Point(91, 10);
+            this.txtSubTotal.Name = "txtSubTotal";
+            this.txtSubTotal.Size = new System.Drawing.Size(19, 20);
+            this.txtSubTotal.TabIndex = 20;
+            this.txtSubTotal.Text = "0";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTotal.AutoSize = true;
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(68, 7);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(19, 20);
+            this.txtTotal.TabIndex = 21;
+            this.txtTotal.Text = "0";
+            // 
+            // groupBoxSubtotal
+            // 
+            this.groupBoxSubtotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBoxSubtotal.Controls.Add(this.txtTituloSubTotal);
+            this.groupBoxSubtotal.Controls.Add(this.txtSubTotal);
+            this.groupBoxSubtotal.Location = new System.Drawing.Point(12, 366);
+            this.groupBoxSubtotal.Name = "groupBoxSubtotal";
+            this.groupBoxSubtotal.Size = new System.Drawing.Size(210, 37);
+            this.groupBoxSubtotal.TabIndex = 22;
+            this.groupBoxSubtotal.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.txtTituloTotal);
+            this.groupBox1.Controls.Add(this.txtTotal);
+            this.groupBox1.Location = new System.Drawing.Point(228, 369);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 34);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
             // 
             // Facturacion
             // 
@@ -194,8 +356,6 @@
             this.Controls.Add(this.lblName);
             this.Name = "Facturacion";
             this.Text = "Facturacion";
-            this.Load += new System.EventHandler(this.Facturacion_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
             this.ClientBox.ResumeLayout(false);
             this.ProductBox.ResumeLayout(false);
             this.ProductBox.PerformLayout();
